9d176682efccb58dd7f4b26ea3290a4a349ad027
Mr. Fluffy <git@sixearsm>
2023-12-17 09:12:51 +0000

    x

--- flake.nix
+++ flake.nix
@@ -4,7 +4,8 @@
   inputs = {
     nixpkgs.url     = github:nixos/nixpkgs/be44bf67; # nixos-22.05 2022-10-15
     flake-utils.url = github:numtide/flake-utils/c0e246b9;
-    hpkgs1.url      = github:sixears/hpkgs1/r0.0.7.0;
+#    hpkgs1.url      = github:sixears/hpkgs1/r0.0.19.0;
+    hpkgs1.url      = "/home/martyn/src/hpkgs1";
   };
 
   outputs = { self, nixpkgs, flake-utils, hpkgs1 }:
