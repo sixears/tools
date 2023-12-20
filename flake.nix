52b813f570809fcd4c2ad25c60c53a4334dcedf6
Mr. Fluffy <git@sixearsm>
2023-12-20 09:33:38 +0000

    use 2023-12-13; ghc9+hpkgs r0.0.24.0

--- flake.nix
+++ flake.nix
@@ -2,14 +2,14 @@
   description = "useful general-purpose tools scripts";
 
   inputs = {
-    nixpkgs.url     = github:nixos/nixpkgs/be44bf67; # nixos-22.05 2022-10-15
+    nixpkgs.url     = github:NixOS/nixpkgs/354184a; # master 2023-12-13
     flake-utils.url = github:numtide/flake-utils/c0e246b9;
-#    hpkgs1.url      = github:sixears/hpkgs1/r0.0.19.0;
-    hpkgs1.url      = "/home/martyn/src/hpkgs1";
+    hpkgs1.url      = github:sixears/hpkgs1/r0.0.24.0;
+#    hpkgs1.url      = "/home/martyn/src/hpkgs1";
   };
 
   outputs = { self, nixpkgs, flake-utils, hpkgs1 }:
-    flake-utils.lib.eachDefaultSystem (system:
+    flake-utils.lib.eachSystem ["x86_64-linux"] (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
 
